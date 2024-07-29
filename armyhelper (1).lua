bbb8e05ade45ff91a85fab10193d9dcc409e2641
coulanderson <asdfghjkliuytfdx204@gmail.com>
2024-07-30 02:46:24 +0300

    Add files via upload

--- armyhelper (1).lua
+++ armyhelper (1).lua
@@ -5,7 +5,31 @@ script_name('ArmyHelper')
 script_version("29.07.2024")
 
 
-local dlstatus = require('moonloader').download_satus
+local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
+local autoupdate_loaded = false
+local Update = nil
+if enable_autoupdate then
+    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
+    if updater_loaded then
+        autoupdate_loaded, Update = pcall(Updater)
+        if autoupdate_loaded then
+            Update.json_url = "https://raw.githubusercontent.com/coulanderson/Army/main/update.%20json" .. tostring(os.clock())
+            Update.prefix = "[" .. string.upper(thisScript().name) .. "]: "
+            Update.url = "https://github.com/coulanderson/Army"
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
 local imgui = require "mimgui"
 local new = imgui.new
 local ffi = require 'ffi'
@@ -39,14 +63,6 @@ encoding.default = "CP1251"
 
 
 
-local script_vers = 1
-local script_vers_text = '1.00'
-
-local update_url = 'https://raw.githubusercontent.com/coulanderson/Army/main/update.ini'
-local update_path = getWorkingDirectory() .. '/update.ini'
-
-local script_url = ''
-local script_path =  thisScript().path
 
 
 
@@ -1030,27 +1046,12 @@ function main()
     while not isSampAvailable() do wait(10000) end
     sampAddChatMessage('ArmyHelper: Готов к работе. Активация: /ah ', 0xFFFFFFF )
     
+     if autoupdate_loaded and enable_autoupdate and Update then
+        pcall(Update.check, Update.json_url, Update.prefix, Update.url)
+    end
     
     
-    
-    downloadUrlToFile(update_url, update_path, function (id, status)
-       
-        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-            uIni = inicfg.load(nil, update_path)
-            if tonumber(uIni.info.vers) > script_vers then
-                sampAddChatMessage('Доступно обновление. Версия:'..uIni.info.vers_text)
-                update_state = true
-            end
-        
-            os.remove(update_path)
-        
-        
-        
-        end
-    end)
-    
-    
-    
+   
     
     
     
@@ -1094,33 +1095,6 @@ function main()
 
        
     while true do wait(0)
-        
-
-        if update_state then
-            
-            downloadUrlToFile(script_url, script_path, function (id, status)
-       
-                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage('fffffff', -1)
-                    thisScript():reload()
-                
-                end
-            end)
-            break
-
-
-
-        end
-
-
-
-
-
-
-
-
-
-
 
 
        local _, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
